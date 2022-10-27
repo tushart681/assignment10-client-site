525ff008f9ddc68c8d50b86f4165665cb48f555c
tushart681 <tushart681@gmail.com>
2022-10-27 22:59:52 +0600

    6th update

--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            
+            <h2>What is cors?</h2>
+            <br />
+            <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
         </div>
     );
 };
