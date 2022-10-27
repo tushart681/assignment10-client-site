7b39d504037c5252c3ef9207318a3339d16529cc
tushart681 <tushart681@gmail.com>
2022-10-27 23:00:39 +0600

    7th update

--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -6,6 +6,8 @@ const Blog = () => {
             <h2>What is cors?</h2>
             <br />
             <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
+            <br />
+            <h2>Why are you using firebase? What other options do you have to implement authentication?</h2>
         </div>
     );
 };
