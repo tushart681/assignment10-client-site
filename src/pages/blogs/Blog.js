8c234a61d86107ef32a9a4318c00c04ec1da1064
tushart681 <tushart681@gmail.com>
2022-10-27 23:02:10 +0600

    8th update

--- src/pages/blogs/Blog.js
+++ src/pages/blogs/Blog.js
@@ -8,6 +8,8 @@ const Blog = () => {
             <p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.</p>
             <br />
             <h2>Why are you using firebase? What other options do you have to implement authentication?</h2>
+            <br />
+            <p>Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together., Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.</p>
         </div>
     );
 };
